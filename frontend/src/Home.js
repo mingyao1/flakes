088a7470d2104b41dc1c8bcb79effc4ae9712e7b
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-09-16 14:06:43 -0500

    init

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return <div className="container">
+        <header>
+            <div>Hello World</div>
+            <nav className="nav"></nav>
+        </header>
+        
+    </div>
+}
+
+export default Home;
