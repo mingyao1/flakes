17bdb9f0d2bd196b6389f3917b9cd77849895ee4
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-09-16 15:02:41 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,4 +8,10 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/api/data')
+def get_data():
+    data = {'message': 'This is JSON data from the backend'}
+    return jsonify(data)
+
 
