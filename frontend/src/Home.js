c04f66bcf3022f281e099719e337484c06cead4c
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-09-16 14:31:15 -0500

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+
+
+    
     return <div className="container">
         <header>
             <div>Hello World</div>
-            <nav className="nav"></nav>
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <Link to='/' className="nav-link">Home</Link>
+                    </li>
+                    <li>
+
+                    </li>
+                </ul>
+            </nav>
         </header>
         
     </div>
