0ffeb88c382b48692ee995fa1371faeeb3070f89
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-09-16 20:10:02 -0500

    Update server.js

--- frontend/src/server/server.js
+++ frontend/src/server/server.js
@@ -1,3 +1,30 @@
 import axios from "axios";
 
+const url = 'http://127.0.0.1:5000'
+
+const search = ( {props} ) => {
+    axios.get(url + '/search', { props }).then((res) => {
+        alert(JSON.stringify(res.data));  
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+};
+
+const getAsset = ({ props }) => {
+    axios.get(url + '/get-assests', { props }).then((res) => {
+        alert(JSON.stringify(res.data));  
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+};
+
+const getProjection = ( {assetid} ) => {
+    //later
+};
+
+
+
 
