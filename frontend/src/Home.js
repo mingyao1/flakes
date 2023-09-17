80ccffa72cebe1e1c2af00fe9d7d84d08f3cc1a4
mingyao1 <mingyaol@smu.edu>
2023-09-17 03:08:41 -0500

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -10,35 +10,18 @@ const Home = () => {
 
     return <div className="bg-light text-white p-3">
         <header>
-            <span style={{ color: 'green' }}>CBRE Management System</span>
+            <span style={{ color: 'green' }}>Flake </span>
             <nav className="nav" style={{ marginLeft: 'auto' }}>
             <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
             <li style={{ marginLeft: '20px' }}>
+            marginLeft
             <Link to='/' className="nav-link">Home</Link>
                     </li>
-                    <Link to='/' className="nav-link">Dashboard</Link>
                     <li>
                     </li>
                 </ul>
             </nav>
         </header>
-
-
-        
-
-        <div>
-        <button type="button" class="btn btn-secondary" data-toggle="Work order" data-placement="received" title="Work order received">
-            Work order received
-        </button>
-        &nbsp;&nbsp;&nbsp; {/* Add non-breaking spaces */}
-        <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="right" title="Repairs applied">
-            Repair applied
-        </button>
-        &nbsp;&nbsp;&nbsp; {/* Add non-breaking spaces */}
-        <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="High risk asset">
-            High risk asset
-        </button>
-        </div>
 
 
         <main>
