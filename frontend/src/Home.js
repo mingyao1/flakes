ec7bcec31a223e8cc5e8078915032e73c61c52a1
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-09-16 22:37:33 -0500

    update

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,29 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
-
-import api from "./server/server";
+import Search from "./Search";
 
 
 const Home = () => {
 
-    const [query, setQuery] = useState('');
-    const [assests, setAssets] = useState([])
-
-    const handle = () => {
-        console.log(query);
-
-        api.search({ props: query })
-            .then((res) => {
-                setAssets(res.data); // Update the assets state with the search result
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }
-
-
+    
 
     return <div className="container">
         <header>
@@ -41,14 +23,7 @@ const Home = () => {
         </header>
 
         <main>
-            <div className="form-field">
-                <input type="text" className="form-control" id="search-input" placeholder="Search..."
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)} />
-                <button className="btn btn-primary" onClick={handle}>Search</button>
-            </div>
-
-
+            <Search />
         </main>
 
     </div>
