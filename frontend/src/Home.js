eff75283eda359a3845c16f15b3d226c63e9a0bb
mingyao1 <122957298+LoganLu2001@users.noreply.github.com>
2023-09-17 05:12:04 -0500

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import Search from "./Search";
 import { PieChart } from 'react-minimal-pie-chart';
 
+const manufacturers = [
+    { title: 'Manufacturer 1', value: 10, color: '#FF0000' },
+    { title: 'Manufacturer 2', value: 15, color: '#FFFF00' },
+    { title: 'Manufacturer 3', value: 20, color: '#800080' },
+    { title: 'Manufacturer 4', value: 12, color: '#FF5733' },
+    { title: 'Manufacturer 5', value: 8, color: '#0099CC' },
+  ];
+
 const Home = () => {
 
     return <div className="bg-light p-3">
@@ -21,13 +29,25 @@ const Home = () => {
 
         <main>
             <Search />
+            <div style={{ width: '50%', display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+            {manufacturers.map((manufacturer, index) => (
+            <div key={`slice-${index}`}>
+            <div style={{ backgroundColor: manufacturer.color, width: '20px', height: '20px', marginRight: '8px' }}></div>
+            {manufacturer.title}
+            </div>
+            ))}
+
+            </div>
             <PieChart
                 style={{ width: '50%' }}
                 data={[
-                    { title: 'One', value: 10, color: '#E38627' },
-                    { title: 'Two', value: 15, color: '#C13C37' },
-                    { title: 'Three', value: 20, color: '#6A2135' },
+                    { title: 'Manufacturer 1', value: 10, color: '#FF0000' },
+                    { title: 'Manufacturer 2', value: 15, color: '#FFFF00' },
+                    { title: 'Manufacturer 3', value: 20, color: '#800080' },
+                    { title: 'Manufacturer 4', value: 12, color: '#FF5733' },
+                    { title: 'Manufacturer 5', value: 8, color: '#0099CC' },
                 ]}
+            
             />
         </main>
 
